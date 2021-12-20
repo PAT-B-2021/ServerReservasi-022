29153b827f48dcd785890fbaf49cf500d2a1125f
LaelaWulida <laela27811@gmail.com>
2021-12-20 23:08:15 +0700

    Add project files.

---
+++ ServerReservasi-20190140022/Program.cs
@@ -0,0 +1,47 @@
+﻿using ServiceReservasi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerReservasi_20190140022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            Uri address = new Uri("http://localhost:8733/Design_Time_Addresses/ServiceReservasi/Service1/");
+            BasicHttpBinding bind = new BasicHttpBinding();
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1), address);
+                //alamat base address
+                hostObj.AddServiceEndpoint(typeof(IService1), bind, "");
+                //alamat endpoint
+                //wsdl
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); //service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dinuka saat development, tidak untuk dibuka)
+                hostObj.Description.Behaviors.Add(smb);
+                //mex
+                Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObj.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!");
+                Console.ReadLine();
+
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
